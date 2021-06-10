5b786a3f27ef1eacaeb4ed288f8881fccffd1fa4
Extra <47879832+ma1833577561@users.noreply.github.com>
2021-06-10 16:48:11 +0800

    Add files via upload

---
+++ src/components/setting/setLeft.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="set-com">
+    <div class="set-title">
+      {{ $t("settingData.level.title") }} :<span>0px</span>
+    </div>
+    <slider></slider>
+  </div>
+</template>
+
+<script>
+import slider from "@/components/config/slider.vue";
+export default {
+  name: "SetLeft",
+  components: {
+    slider
+  }
+};
+</script>
---
+++ src/components/setting/setTop.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="set-com">
+    <div class="set-title">
+      {{ $t("settingData.vertical.title") }} :<span>0px</span>
+    </div>
+    <slider></slider>
+  </div>
+</template>
+
+<script>
+import slider from "@/components/config/slider.vue";
+export default {
+  name: "SetTop",
+  components: {
+    slider
+  }
+};
+</script>
